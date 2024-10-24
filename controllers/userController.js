5325a198c882437a2c7580484d1017a353c50e8a
insoo00 <insoosos1234@gmail.com>
2024-10-25 00:36:42 +0900

    [Refactor] Move authRoutes to userRoutes/

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models');
+const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
   try {
@@ -10,4 +11,20 @@ exports.createUser = async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
+};
+
+exports.getUser = async (req, res) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (!token) return res.status(403).send('No token provided.');
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(500).send('Failed to authenticate token.');
+    res.json({
+      userId: decoded.userId,
+      name: decoded.name,
+      email: decoded.email,
+      profileImgUrl: decoded.profileImgUrl,
+    });
+  });
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -30,19 +30,4 @@ router.get(
   }
 );
 
-router.get('/me', (req, res) => {
-  const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) return res.status(403).send('No token provided.');
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(500).send('Failed to authenticate token.');
-    res.json({
-      userId: decoded.userId,
-      name: decoded.name,
-      email: decoded.email,
-      profileImgUrl: decoded.profileImgUrl,
-    });
-  });
-});
-
 module.exports = router;
