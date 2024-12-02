64802e40ca0e1aa8436002e77c11aa5af3025cd5
insoo00 <insoosos1234@gmail.com>
2024-12-03 00:07:29 +0900

    [Fix] accessToken으로 변경

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,10 +3,11 @@ const jwt = require('jsonwebtoken');
 exports.verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
 
-  if (!token) return res.status(403).send('No token provided.');
+  if (token == null || token == 'undefined')
+    return res.status(401).send('No token provided.');
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(500).send('Failed to authenticate token.');
+    if (err) return res.status(403).send('Failed to authenticate token.');
     req.user = decoded;
     next();
   });
