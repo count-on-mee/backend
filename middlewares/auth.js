0cc0ededed4cd0175632bcbda228d72bae55b60e
insoo00 <insoosos1234@gmail.com>
2024-12-03 00:11:22 +0900

    [Feat] auth 미들웨어 추가

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,3 +12,21 @@ exports.verifyToken = (req, res, next) => {
     next();
   });
 };
+
+exports.optionalVerifyToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (token == null || token == 'undefined') {
+    req.user = null; // 비로그인 사용자 처리
+    return next();
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      req.user = null; // 토큰 인증 실패 시 비로그인 사용자로 처리
+    } else {
+      req.user = decoded;
+    }
+    next();
+  });
+};
