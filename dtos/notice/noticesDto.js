b99dc2843c9f291007150611b88d3e7d7731f831
insoo00 <insoosos1234@gmail.com>
2024-12-11 16:04:34 +0900

    [Fix] 공지사항 응답에 날짜 추가

--- dtos/notice/noticesDto.js
+++ dtos/notice/noticesDto.js
@@ -1,11 +1,13 @@
 class NoticesDto {
   static of(notices) {
     return notices.map((notice) => {
-      const { noticeId, title, content } = notice;
+      const { noticeId, title, content, created_at } = notice;
+      const createdAt = new Date(created_at).toISOString().split('T')[0];
       return {
         noticeId,
         title,
         content,
+        createdAt,
       };
     });
   }
