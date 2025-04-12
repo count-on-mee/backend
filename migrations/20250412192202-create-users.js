616283357b902c9c4d7b5b7ec6148f816b8ef280
insoo00 <insoosos1234@gmail.com>
2025-04-13 04:43:43 +0900

    🗃️ User 테이블 생성 #5

---
+++ migrations/20250412192202-create-users.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      user_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      nickname: {
+        type: Sequelize.STRING(30),
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      profile_img_url: {
+        type: Sequelize.STRING(500),
+        allowNull: true,
+      },
+      oauth_provider: {
+        type: Sequelize.ENUM('google', 'naver', 'kakao'),
+        allowNull: false,
+      },
+      oauth_id: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    // 복합 유니크 인덱스 추가
+    await queryInterface.addIndex('user', ['oauth_provider', 'oauth_id'], {
+      unique: true,
+      name: 'uk_oauth_provider_oauth_id',
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user');
+  },
+};
---
+++ models/user.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      nickname: {
+        type: DataTypes.STRING(30),
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      profileImgUrl: {
+        type: DataTypes.STRING(500),
+        allowNull: true,
+      },
+      oauthProvider: {
+        type: DataTypes.ENUM('google', 'naver', 'kakao'),
+        allowNull: false,
+      },
+      oauthId: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'user',
+      timestamps: true,
+      underscored: true,
+      indexes: [
+        {
+          unique: true,
+          fields: ['oauth_provider', 'oauth_id'],
+          name: 'uk_oauth_provider_oauth_id',
+        },
+      ],
+    }
+  );
+
+  return User;
+};
