4e546428262cd21abc3c79a4323a184a4384779a
insoo00 <insoosos1234@gmail.com>
2025-04-15 21:09:31 +0900

    ✨ RefreshToken 검증 미들웨어 구현 #8

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,6 +6,11 @@ const validateAccessToken = (authHeader) => {
   return { userId: decoded.userId };
 };
 
+const validateRefreshToken = (refreshToken) => {
+  const decoded = JwtUtil.verifyToken(refreshToken, true);
+  return { userId: decoded.userId };
+};
+
 exports.requireAuth = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
@@ -35,6 +40,21 @@ exports.optionalAuth = (req, res, next) => {
 
     // 토큰이 있는 경우: 로그인 사용자
     req.user = validateAccessToken(authHeader);
+    next();
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
+exports.refreshTokenAuth = (req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return res.status(401).json({ message: '로그인 정보가 없습니다.' });
+    }
+
+    req.user = validateRefreshToken(refreshToken);
     next();
   } catch (error) {
     res.status(401).json({ message: error.message });
