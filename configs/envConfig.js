8c56453e85dbd9f9014c48be9c7e379ead010acb
insoo00 <insoosos1234@gmail.com>
2025-05-28 13:47:17 +0900

    ✏️ 환경변수명 수정 #36

--- configs/envConfig.js
+++ configs/envConfig.js
@@ -74,7 +74,6 @@ const validateRequiredEnvVars = () => {
     'AWS_ACCESS_KEY_ID',
     'AWS_SECRET_ACCESS_KEY',
     'AWS_REGION',
-    'AWS_S3_BUCKET',
     'AWS_BUCKET_NAME',
 
     // Naver Cloud Platform
